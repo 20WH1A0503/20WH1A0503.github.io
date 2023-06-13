e6ed2a39552f6102769ba2e6a77dde9f5fe51fb4
20WH1A0503 <94287227+20WH1A0503@users.noreply.github.com>
2023-06-13 21:55:42 +0530

    typo

--- usercard3.js
+++ usercard3.js
@@ -10,7 +10,7 @@ let displayobject;
 let getrandomuser=function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data=>console.log(data))
     document.getElementById("rohit-img").src = displayobject.imgurl;
     document.getElementById("rohit").innerHTML = displayobject.name;
     document.getElementById("rohit-desc").innerHTML = displayobject.Description;

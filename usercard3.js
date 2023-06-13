1c37b5c9069357c30a94ac7035dd31cc6ff7f39a
20WH1A0503 <94287227+20WH1A0503@users.noreply.github.com>
2023-06-13 21:26:02 +0530

    Created usercard3.html,css,js

---
+++ usercard3.js
@@ -0,0 +1,26 @@
+let CaptainRohit={
+    imgurl:"https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2023/6.png",
+    name:"RohitSharma",
+    Description:"Captain of Mumbai Indians"
+
+}
+let youngRohit ={
+    imgurl:"https://static.toiimg.com/thumb/imgsize-23456,msid-2876942,width-600,resizemode-4/2876942.jpg",
+    name:"Rohit Gurunath Sharma",
+    Description:"Cricketer"
+}
+let isCaptainRohit=true;
+let displayobject;
+let flipdata=function(){
+    if(isCaptainRohit == true){
+        displayobject=youngRohit;
+        isCaptainRohit=false;
+    }
+    else{
+        displayobject=CaptainRohit;
+        isCaptainRohit=true;
+    }
+    document.getElementById("rohit-img").src = displayobject.imgurl;
+    document.getElementById("rohit").innerHTML = displayobject.name;
+    document.getElementById("rohit-desc").innerHTML = displayobject.Description;
+}
